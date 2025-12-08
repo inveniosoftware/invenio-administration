28e5feed96087e7644e126fec39105b7d4194319
Sam Arbid <samaa2@kth.se>
2025-12-08 12:38:51 +0100

    📦 release: v4.3.2

--- invenio_administration/__init__.py
+++ invenio_administration/__init__.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2022-2024 CERN.
 # Copyright (C) 2024-2025 Graz University of Technology.
+# Copyright (C) 2024-2025 KTH Royal Institute of Technology.
 #
 # invenio-administration is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -11,6 +12,6 @@
 
 from .ext import InvenioAdministration
 
-__version__ = "4.3.1"
+__version__ = "4.3.2"
 
 __all__ = ["InvenioAdministration"]
