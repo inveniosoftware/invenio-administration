0678aae17ce8a66e6f9bbd49654c7df144bc3f85
Sam Arbid <samaa2@kth.se>
2026-08-26 11:14:18 +0200

    📦 release: v6.1.1

--- invenio_administration/__init__.py
+++ invenio_administration/__init__.py
@@ -1,12 +1,12 @@
 # SPDX-FileCopyrightText: 2022-2024 CERN.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
-# SPDX-FileCopyrightText: 2024-2025 KTH Royal Institute of Technology.
+# SPDX-FileCopyrightText: 2024-2026 KTH Royal Institute of Technology.
 # SPDX-License-Identifier: MIT
 
 """Invenio module providing extensible administration panel."""
 
 from .ext import InvenioAdministration
 
-__version__ = "6.1.0"
+__version__ = "6.1.1"
 
 __all__ = ["InvenioAdministration"]
